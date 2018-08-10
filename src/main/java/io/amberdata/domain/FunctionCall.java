1d4806b54a32a087433b0782ffda910e8545db17
Dmitriy Butakov <dmitry404@gmail.com>
2018-08-10 14:32:59 +0300

    makes FunctionCall / Asset entities to implement common interface with the rest of entities

--- src/main/java/io/amberdata/domain/FunctionCall.java
+++ src/main/java/io/amberdata/domain/FunctionCall.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-public class FunctionCall {
+public class FunctionCall implements BlockchainEntity {
 
   private String name;
   private String hash;
