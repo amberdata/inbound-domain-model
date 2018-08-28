51d4857ae831f9d8c59412c8c72ac121d105351d
Joanes Espanol <joanesespanol@users.noreply.github.com>
2018-08-28 14:02:00 -0700

    Update TransactionState.java

--- src/main/java/io/amberdata/ingestion/domain/TransactionState.java
+++ src/main/java/io/amberdata/ingestion/domain/TransactionState.java
@@ -2,29 +2,29 @@ package io.amberdata.ingestion.domain;
 
 public enum TransactionState {
 
-  unknown(0),
-  pending(1),
-  confirmed(2),
-  replaced(3),
-  lost(4);
+  unknown   (0),
+  pending   (1),
+  confirmed (2),
+  replaced  (3),
+  lost      (4);
 
   private final int id;
 
-  TransactionState(int id) {
+  TransactionState (int id) {
     this.id = id;
   }
 
-  public int getId() {
+  public int getId () {
     return this.id;
   }
 
-  public static TransactionState fromId(Integer id) {
-    if (id == null) {
+  public static TransactionState fromId (Integer id) {
+    if ( id == null ) {
       return null;
     }
 
     for (TransactionState type : values()) {
-      if (type.getId() == id) {
+      if ( type.getId() == id ) {
         return type;
       }
     }
